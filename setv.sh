b0752c2c54ee3b755ac327a8828b02c110f8a6ca
Nathan Duckett <nduckett13@gmail.com>
2020-05-11 14:54:26 +1200

    Add full root path to linux-versions folder

--- setv.sh
+++ setv.sh
@@ -7,4 +7,8 @@ if [ $# -ne 2 ]; then
     return
 fi
 
-. ./linux-versions/$1.sh $2
+# Oneliner from https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+# This will get the root directory of where the script is stored.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+. $DIR/linux-versions/$1.sh $2
