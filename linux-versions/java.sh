eaa9c97a0c4f0ee2726b2be41f893fd48d7b0d36
Nathan Duckett <nduckett13@gmail.com>
2020-05-11 17:12:49 +1200

    Ensure java bin path is first

--- linux-versions/java.sh
+++ linux-versions/java.sh
@@ -8,6 +8,6 @@ if [ $# -ne 1 ]; then
 fi
 
 export JAVA_HOME="/usr/lib/jvm/java-$1-openjdk-amd64"
-export PATH="$PATH:$JAVA_HOME/bin"
+export PATH="$JAVA_HOME/bin:$PATH"
 
 echo "Java version successfully set to $1"
