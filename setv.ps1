33157c133bbe8bbb7772b64d07815c59fedb9293
Nathan Duckett <nduckett13@gmail.com>
2020-05-13 17:08:45 +1200

    Update powershell execution with default to Java 11

--- setv.ps1
+++ setv.ps1
@@ -1,8 +1,8 @@
 # Author: Nathan Duckett
 
-if (args.length != 2) {
+if ($args.length != 2) {
     Write-Output "Invalid arguments used: Expects setv <application_name> <application_version>"
     return
 }
 
-. ./windows-versions/args[0].ps1 args[1]
+. ./windows-versions/$args[0].ps1 -version $args[1]
--- windows-versions/java.ps1
+++ windows-versions/java.ps1
@@ -4,12 +4,13 @@ jdk_8_path="C:\Program Files\Java\jdk1.8.0_251"
 jdk_11_path="C:\Program Files\Java\jdk-11.0.7"
 jdk_14_path="C:\Program Files\Java\jdk-14.0.1"
 
-if (args.length != 1) {
-    Write-Output "Invalid arguments expects: version_number"
-    return
-}
+# Default to Java 11
+param(
+    [string]$version = "11"
+)
 
-switch(args[0]) {
+# Switch the version extracted from parameters
+switch($version) {
     8 { java_path=$jdk_8_path }
     11 { java_path=$jdk_11_path }
     14 { java_path=$jdk_14_path }
